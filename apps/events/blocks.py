e1a36962d125fff04f21248bd18c411aee0f243a
vellip <vellerphilipp@googlemail.com>
2017-02-24 16:50:53 +0100

    add block for display on home to tease events

---
+++ apps/events/blocks.py
@@ -0,0 +1,23 @@
+from wagtail.wagtailadmin import blocks
+
+from apps.events.models import EventPage
+
+
+class EventsTeaserBlock(blocks.StructBlock):
+    heading = blocks.CharBlock(required=False)
+    count = blocks.IntegerBlock(min_value=2,
+                                help_text='How many events should be '
+                                          'shown?')
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context.update({
+            'events': EventPage.objects.live()[:value['count']]
+        })
+
+        return context
+
+    class Meta:
+        template = 'events/blocks/eventsteaser_block.html'
+
+
--- apps/home/models.py
+++ apps/home/models.py
@@ -1,5 +1,17 @@
+from wagtail.wagtailadmin import blocks
+from wagtail.wagtailadmin import edit_handlers
+from wagtail.wagtailcore import fields
 from wagtail.wagtailcore.models import Page
+
+from apps.events.blocks import EventsTeaserBlock
 
 
 class HomePage(Page):
-    pass
+    body = fields.StreamField([
+        ('events_list', EventsTeaserBlock(icon='date')),
+        ('text', blocks.RichTextBlock())
+    ])
+
+    content_panels = Page.content_panels + [
+        edit_handlers.StreamFieldPanel('body')
+    ]
