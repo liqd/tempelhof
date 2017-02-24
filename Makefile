9c25f226949995a52238ac89676e10e366e5c5f3
vellip <vellerphilipp@googlemail.com>
2017-02-24 14:28:22 +0100

    setup basic frontend and webpack process

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 VIRTUAL_ENV ?= .env
 NODE_BIN = node_modules/.bin
+SCSS_FILES := $(shell find 'tempelhof/assets/scss' -name '*.scss')
+JS_FILES := $(shell find 'tempelhof/assets/js' | grep '\.jsx\?$$')
 
 install:
 	npm install
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var webpack = require('webpack')
+var path = require('path')
+var autoprefixer = require('autoprefixer')
+
+module.exports = {
+  entry: {
+    tempelhof: [
+      './tempelhof/assets/scss/style.scss',
+      './tempelhof/assets/js/app.js'
+    ],
+    vendor: [
+      'font-awesome/scss/font-awesome.scss',
+      'jquery',
+    ]
+  },
+  devtool: 'eval',
+  output: {
+    libraryTarget: 'var',
+    library: 'tempelhof',
+    path: './tempelhof/static/',
+    publicPath: '/static/',
+    filename: '[name].js'
+  },
+  externals: {
+    'django': 'django'
+  },
+  module: {
+    noParse: /\.min\.js$/,
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules\/(?!adhocracy4|bootstrap)/,  // exclude all dependencies but adhocracy4 and bootstrap
+        loader: 'babel-loader',
+        query: {
+          presets: ['babel-preset-es2015', 'babel-preset-react'].map(require.resolve)
+        }
+      },
+      {
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract('style?sourceMap', '!css?sourceMap!postcss?sourceMap!sass?sourceMap')
+      },
+      {
+        test: /fonts\/.*\.(svg|woff2?|ttf|eot)(\?.*)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.svg$|\.png$/,
+        loader: 'file-loader?name=images/[name].[ext]'
+      }
+    ]
+  },
+  postcss: [
+    autoprefixer({browsers: ['last 3 versions', 'ie >= 10']})
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss', '.css'],
+    alias: {
+      'jquery$': 'jquery/dist/jquery.min.js'
+    },
+    // when using `npm link`, dependencies are resolved against the linked
+    // folder by default. This may result in dependencies being included twice.
+    // Setting `resolve.root` forces webpack to resolve all dependencies
+    // against the local directory.
+    root: path.resolve('./node_modules')
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new ExtractTextPlugin('[name].css')
+  ]
+}
