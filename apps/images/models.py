682dc4ba0634f85f97e97a567dd60b345a82f33a
vellip <vellerphilipp@googlemail.com>
2017-03-20 11:56:05 +0100

    fix lint error

--- apps/images/models.py
+++ apps/images/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from wagtail.wagtailimages.models import Image, AbstractImage, AbstractRendition
+from wagtail.wagtailimages.models import Image, AbstractImage, \
+    AbstractRendition
 
 
 class CustomImage(AbstractImage):
