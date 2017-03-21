60ddf0f8bbc31e8e289ff10171592a55690efbfa
vellip <vellerphilipp@googlemail.com>
2017-03-21 12:59:43 +0100

    re-add calendar js

--- tempelhof/assets/js/app.js
+++ tempelhof/assets/js/app.js
@@ -1,5 +1,6 @@
 // lightbox needs the jQuery object in global scope
 require('./jquery-fix.js')
+const calendar = require('events/static/events/calendar.js')
 const collapse = require('./collapse.js')
 const menu = require('./menu')
 const lightbox = require('./lightbox')
