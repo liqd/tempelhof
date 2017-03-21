1c8e0f263a4d09f02d6dd5483b83963f7b5f8f2e
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-03-21 15:27:32 +0100

    merge migrations

---
+++ apps/docs/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('docs', '0003_verbose_name'),
+        ('docs', '0003_add-custom-image'),
+    ]
+
+    operations = [
+    ]
---
+++ apps/events/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0004_verbose_name'),
+        ('events', '0004_add-custom-image'),
+    ]
+
+    operations = [
+    ]
