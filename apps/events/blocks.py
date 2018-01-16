d58e3178939532ddb5802c2d2f9b2ae42eed3a9f
fuzzylogic2000 <katharina.albers@liqd.de>
2018-01-16 13:57:53 +0100

    do not use templatetag to get next five upcoming events in block

--- apps/events/blocks.py
+++ apps/events/blocks.py
@@ -1,4 +1,7 @@
+from django.utils import timezone
 from wagtail.wagtailcore import blocks
+
+from apps.events.models import EventPage
 
 
 class EventsTeaserBlock(blocks.StructBlock):
@@ -8,6 +11,22 @@ class EventsTeaserBlock(blocks.StructBlock):
         required=False,
         target_model='events.CalendarPage'
     )
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        events = EventPage.objects.live()
+
+        events = events.filter(date__gte=timezone.now())
+        events = events.order_by('date', 'time_start')
+
+        if events and events.count() >= 5:
+            events = events[:5]
+
+        context.update({
+            'events': events
+        })
+
+        return context
 
     class Meta:
         template = 'events/blocks/eventsteaser_block.html'
