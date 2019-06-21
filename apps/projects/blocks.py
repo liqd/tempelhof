f2d51e2d81fb8e726a43a686a264d127c982da79
fuzzylogic2000 <katharina.albers@liqd.de>
2019-06-21 10:19:45 +0200

    apps/projects/block: make only ProjectPages choosable

--- apps/projects/blocks.py
+++ apps/projects/blocks.py
@@ -12,7 +12,10 @@ class CurrentProjectBlock(blocks.PageChooserBlock):
 
 class CurrentProjectsListBlock(blocks.StructBlock):
     title = blocks.CharBlock()
-    projects = blocks.ListBlock(CurrentProjectBlock)
+    projects = blocks.ListBlock(
+        CurrentProjectBlock,
+        target_model='projects.ProjectPage'
+    )
 
     class Meta:
         template = 'projects/blocks/projectslist_block.html'
