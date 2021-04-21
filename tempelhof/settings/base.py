e827d121091fa4a8f28e1572612ed39edd3051fb
phillimorland <phillippamorland@gmail.com>
2021-04-21 13:33:47 +0200

    base.py//base.txt: rm wagtail middleware and update version

--- tempelhof/settings/base.py
+++ tempelhof/settings/base.py
@@ -70,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
 
-    'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
