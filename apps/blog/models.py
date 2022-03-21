39798c2f50ca8934a1a0f9b3bc60bda7184e9f7b
rine <k.schmid@liqd.net>
2022-03-21 13:06:16 +0100

    django 3.2 update: fix deprecation of ugettext

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from wagtail.admin import edit_handlers
 from wagtail.core import fields as wagtail_fields
 from wagtail.core.models import Page
